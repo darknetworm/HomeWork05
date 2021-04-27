dbf9fa99144fada606237d1a4e3db5d32608093e
Paul <82410807+darknetworm@users.noreply.github.com>
2021-04-27 17:02:31 +0000

    Вывод дохода сотрудников и подсчет средней величины дохода

--- homeWork03.py
+++ homeWork03.py
@@ -4,7 +4,7 @@ count = 0
 
 try:
     with open('forHomeWork03.txt') as salary:
-        print('Оклад менее 20000 имеют следующие сотрудники:')
+        print('Оклад менее 20000 Пиастров имеют следующие сотрудники:')
         for line in salary.readlines():
             userSalary = float(line.split()[1])
             if userSalary < 20000:
