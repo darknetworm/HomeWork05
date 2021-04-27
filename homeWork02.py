b78fd8d0e98d4959e51b5049f46e1327e383b404
Paul <82410807+darknetworm@users.noreply.github.com>
2021-04-27 16:58:41 +0000

    Update homeWork02.py

--- homeWork02.py
+++ homeWork02.py
@@ -5,7 +5,7 @@ try:
         for line in reading.readlines():
             count += 1
             print(
-                'Строка {}: "{}". Количество слов в строке: {}'.format(count, line.replace('\n', ''),
+                'Строка {}: "{}" Количество слов в строке: {}'.format(count, line.replace('\n', ''),
                                                                        len(line.split())))
     print(f'Итого в файле {count} строк')
 
