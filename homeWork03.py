61eaa26a660b347b8f70445a816534dad65a1fe1
Paul <82410807+darknetworm@users.noreply.github.com>
2021-04-27 16:40:45 +0000

    Вывод дохода сотрудников и подсчет средней величины дохода

---
+++ homeWork03.py
@@ -0,0 +1,17 @@
+users = {}
+usersSalary = 0
+count = 0
+
+try:
+    with open('forHomeWork03.txt') as salary:
+        print('Оклад менее 20000 имеют следующие сотрудники:')
+        for line in salary.readlines():
+            userSalary = float(line.split()[1])
+            if userSalary < 20000:
+                print(line.split()[0])
+            usersSalary += userSalary
+            count += 1
+    print(f'\nСредний оклад {count} сотруднuков составляет {usersSalary / count:.2f} Пиастров')
+
+except IOError:
+    print('Где-то закралась ошибка')
